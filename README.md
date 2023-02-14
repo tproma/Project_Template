9cbdd83228e654c3bc9589ecd162f039e770be52
Tanjina <tanjinaproma@gnail.com>
2023-02-14 20:37:04 +0600

    structure created

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Project_Template
-###ML project template for end-to-end industry ready projects
+## ML project template for end-to-end industry ready projects
+
+
+create python environment
+"""
+conda create -p venv python==3.8 -y
+"""
+
+install requirements
+"""
+pip install -r requirements.txt
+"""
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+def get_requirements()->List[str]:
+
+    requirement_list:List[str] = []
+
+    #write code to read requirements.txt file and append each requirements_list variables.
+
+    return requirement_list
+
+
+setup(
+    name="project-package",
+    version = "0.0.1",
+    author= " Proma",
+    author_email="tanjinaproma@gmail.com",
+    packages=find_packages(),
+    install_requires= get_requirements(),          #["pymongo==4.2.0"],
+
+)
